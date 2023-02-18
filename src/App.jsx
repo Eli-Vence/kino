a11f701731bd459be64fb9fe63986031a059d62c
Ilya Rybin <rybinil1998@gmail.com>
2023-02-18 19:53:55 +0300

    List of films

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
-function App() {
-  return (
-    <div className="App">
-     Привет React 
-    </div>
-  )
+import axios from 'axios';
+import React from 'react';
+import Movie from './Movie';
+import Preloader from './Preloader';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section>
+        {isLoading ? (
+          <Preloader />
+        ) : (
+          movies.map((movie) => {
+            return (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            );
+          })
+        )}
+      </section>
+    );
+  }
 }
 
-export default App
+export default App;
